4394d7e98c9c5da6a65f505fbdab75e4ddacd038
Jono Mingard <reason.koan@gmail.com>
2017-09-02 11:53:02 +1200

    Render a create form

--- src/Resource/Render/Basic.elm
+++ src/Resource/Render/Basic.elm
@@ -1,6 +1,8 @@
 module Resource.Render.Basic exposing (..)
 
 import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
 import Resource exposing (..)
 
 
@@ -51,5 +53,52 @@ fieldView field =
 
 
 createForm : FieldBuilder r u -> Model r -> Html (Msg r u)
-createForm _ _ =
-    form [] []
+createForm fields model =
+    let
+        newFields =
+            fields model.new
+    in
+        Html.form [] (List.map (fieldInput model.new) newFields ++ [ createButton ])
+
+
+fieldInput : r -> Field u -> Html (Msg r u)
+fieldInput resource field =
+    case field of
+        TextField title content op ->
+            div []
+                [ label [] [ text title ]
+                , input [ type_ "text", value content, onInput (op >> Update resource) ] []
+                ]
+
+        TextArea title content op ->
+            div []
+                [ label [] [ text title ]
+                , textarea [ onInput (op >> Update resource) ] [ text content ]
+                ]
+
+        NumberField title content op ->
+            div []
+                [ label [] [ text title ]
+                , input [ type_ "number", onInput (handleNumber content >> op >> Update resource) ] [ text (toString content) ]
+                ]
+
+        CheckboxField title content op ->
+            div []
+                [ label [] [ text title ]
+                , input [ type_ "checkbox", checked content, onClick (op (not content) |> Update resource) ] []
+                ]
+
+
+handleNumber : Float -> String -> Float
+handleNumber current new =
+    case String.toFloat new of
+        Err _ ->
+            current
+
+        Ok f ->
+            f
+
+
+createButton : Html (Msg r u)
+createButton =
+    button [ type_ "submit", onClick Create ] [ text "Create" ]
